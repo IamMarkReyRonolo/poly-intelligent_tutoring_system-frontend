ca46004d519ec97948b23a28844d99138f44996f
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2021-12-22 12:49:07 +0800

    minor commits

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-app id="inspire">
+		<router-view></router-view>
+	</v-app>
+</template>
+
+<script>
+	export default {
+		name: "App",
+
+		data: () => ({
+			drawer: false,
+		}),
+	};
+</script>
